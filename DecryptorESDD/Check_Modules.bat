7ab708960acafd9c06ea2d637ec04167bbae6356
DharkonSK <121519654+DharkonSK@users.noreply.github.com>
2023-05-12 13:48:45 -0600

    Delete DecryptorESDD directory

--- DecryptorESDD/Check_Modules.bat
+++
@@ -1,7 +0,0 @@
-:: ESDD Decryptor System File
-@echo off
-if exist "Control Panel.dll" (echo.) else (set systemfileerrorlevel=SevereErrorLevel3)
-if exist "ZLibRunner.dll" (echo.) else (set systemfileerrorlevel=SevereErrorLevel4)
-if exist "YShell.dll" (echo.) else (set systemfileerrorlevel=SevereErrorLevel5)
-if exist "ADZP 20 Complex.sys" (echo.) else (set systemfileerrorlevel=Critical)
-goto :eof
